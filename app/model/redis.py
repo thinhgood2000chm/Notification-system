6805cb9ae7898b4340dd3518acca2ff80399c1cf
thinhnx <thinhnx@seatechit.com.vn>
2023-08-23 15:47:22 +0700

    update yaml

--- app/model/redis.py
+++ app/model/redis.py
@@ -7,10 +7,12 @@ load_dotenv()
 
 
 async def redis_pool():
+    print(f"redis://{os.getenv('HOST_REDIS', 'localhost')}", f"{os.getenv('PASSWORD_REDIS', '')}")
     redis = await aioredis.from_url(
         f"redis://{os.getenv('HOST_REDIS', 'localhost')}",
-        username=f"{os.getenv('USERNAME_REDIS', '')}",
+        # username=f"{os.getenv('USERNAME_REDIS', '')}",
         password=f"{os.getenv('PASSWORD_REDIS', '')}",
         encoding="utf-8", decode_responses=True
     )
+    print(redis)
     return redis
