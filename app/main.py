44346fc0c7983654d4b3bd3ac3b942a108d705f3
thinhnx <thinhnx@seatechit.com.vn>
2023-11-08 15:32:59 +0700

    f

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import os
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import router as v1_router
 
 from app.api.v1.setting.event import create_start_app_handler, create_stop_app_handler
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 load_dotenv('.env')
 app = FastAPI(
@@ -24,6 +26,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+templates = Jinja2Templates(directory=r"D:\test\Notification-system\app\templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def maintenance_notice(request: Request):
+    print("vaof ")
+    return templates.TemplateResponse("noti.html", {"request": request})
 
 app.add_event_handler("startup", create_start_app_handler(app))
 app.add_event_handler("shutdown", create_stop_app_handler(app))
